2561dc6a1c5c6a63647add0176b95e5c2edea356
Daniel Foxyz <145469300+foxynhoz@users.noreply.github.com>
2026-03-08 05:42:06 -0300

    Conection with .env sucessfull

--- FoxtrotProject/FoxtrotProject/Program.cs
+++ FoxtrotProject/FoxtrotProject/Program.cs
@@ -1,1 +1,12 @@
 ﻿using DotNetEnv;
+using Telegram.Bot;
+
+Env.Load("../../../.env");
+
+string token = Environment.GetEnvironmentVariable("TELEGRAM_TOKEN");
+
+var bot = new TelegramBotClient(token);
+
+var me = await bot.GetMe();
+
+Console.WriteLine($"Bot conectado: @{me.Username}");
