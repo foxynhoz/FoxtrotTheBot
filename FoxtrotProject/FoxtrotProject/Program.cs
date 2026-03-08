cbe9b367f5d7b9be01098dac7a4df8c3c82b9591
Daniel Foxyz <145469300+foxynhoz@users.noreply.github.com>
2026-03-08 05:46:44 -0300

    Repo Details Updated

--- FoxtrotProject/FoxtrotProject/Program.cs
+++ FoxtrotProject/FoxtrotProject/Program.cs
@@ -1,12 +1,14 @@
 ﻿using DotNetEnv;
 using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
 
 Env.Load("../../../.env");
 
 string token = Environment.GetEnvironmentVariable("TELEGRAM_TOKEN");
 
 var bot = new TelegramBotClient(token);
-
 var me = await bot.GetMe();
-
 Console.WriteLine($"Bot conectado: @{me.Username}");
